88c7da4261c37ed7202422349c878e7f3fa5e0a4
Crossing Dead Development <QQKITTY2003@GMAIL.COM>
2026-06-21 17:41:51 +0800

    Fix typo: E行 → E型次元短劍

--- src/main/java/me/yisang/limbusegogift/gifts/ETypeDimensionalDagger.java
+++ src/main/java/me/yisang/limbusegogift/gifts/ETypeDimensionalDagger.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class ETypeDimensionalDagger extends BaseAccessory {
     private final Random rng = new Random();
     public ETypeDimensionalDagger(LimbusEGOGift plugin) {
-        super(plugin, "e_type_dimensional_dagger", "E行次元短劍", "&7攻擊時 25% 機率傳送到目標身後並造成額外傷害");
+        super(plugin, "e_type_dimensional_dagger", "E型次元短劍", "&7攻擊時 25% 機率傳送到目標身後並造成額外傷害");
     }
     @Override public void onAttack(EntityDamageByEntityEvent event, Player attacker) {
         double m = plugin.getUpgradeMultiplier(attacker, getId());
