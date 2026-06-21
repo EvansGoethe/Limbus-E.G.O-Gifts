a94eea252e16dbbaae17b33d6eb715a88aa89a95
Crossing Dead Development <QQKITTY2003@GMAIL.COM>
2026-06-21 12:17:02 +0800

    Fix item name: 通行證 -> 通行券

--- src/main/java/me/yisang/limbusegogift/gifts/LaManchalandStandardPass.java
+++ src/main/java/me/yisang/limbusegogift/gifts/LaManchalandStandardPass.java
@@ -6,7 +6,7 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 public class LaManchalandStandardPass extends BaseAccessory {
     public LaManchalandStandardPass(LimbusEGOGift plugin) {
-        super(plugin, "la_manchaland_standard_pass", "拉．曼查樂園常規通行證",
+        super(plugin, "la_manchaland_standard_pass", "拉．曼查樂園常規通行券",
                 "&#FCD05C", "偉大的收尾人追求夢想的故事。",
                 "被動：速度 I");
     }
